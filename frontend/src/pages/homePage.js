a5eb1cd85fcb0c315e85131770400080f3532d4b
Emanuel Moreno <emanuellmorreno@gmail.com>
2025-07-29 22:17:32 -0500

    Added Button

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@mui/material';
 
 const homePage = () => {
-      return (
-        <div>
-          <h1>Welcome to My React App!</h1>
-          <p>This is the home page content.</p>
-        </div>
-      );
-    };
+  return (
+    <>
+      <Button variant="contained" color="primary">Click Me</Button>
+      <div>
+        <h1>Welcome to My React App!</h1>
+        <p>This is the home page content.</p>
+      </div>
+    </>
+  );
+};
 
-    export default homePage;
+export default homePage;
